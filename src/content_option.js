ed655a76e6228d5705af8ce8574ca71e1a318fd1
Prakash <ts-jay.prakash@rakuten.com>
2024-11-15 23:07:08 +0900

    added initial setup

---
+++ src/content_option.js
@@ -0,0 +1,205 @@
+const logotext = "Jay";
+const meta = {
+  title: "Jay K",
+  description:
+    "I’m Jay AI enthusiastic _ Full stack devloper,currently working in Tokyo",
+};
+
+const introdata = {
+  title: "I’m Jay Prakash",
+  animated: {
+    first: "I love coding and writing tomorrow's code today.",
+    second: "I craft tools for real-life solutions.",
+    third: "I develop intelligent applications with generative AI.",
+  },
+  description:
+    "A Tech Lead with 10+ years in software development, specializing in e-Discovery, forensic, and cyber security domain. Based in Tokyo, I excel in full-stack engineering, AI, Web development and digital transformation. I've led teams at Rakuten, Deloitte, and more, earning accolades like the Project Excellence Award. Passionate about mentoring and innovation, I design scalable, high-performance systems. Explore my work and let's connect to drive tech excellence together!",
+  your_img_url: "/jay.jpg",
+  //your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "A bit about my self",
+  aboutme:
+    "A Tech Lead with 10+ years in software development, specializing in e-Discovery, forensic, and cyber security domain. Based in Tokyo, I excel in full-stack engineering, AI, Web development and digital transformation. I've led teams at Rakuten, Deloitte, and more, earning accolades like the Project Excellence Award. Passionate about mentoring and innovation, I design scalable, high-performance systems. Explore my work and let's connect to drive tech excellence together!",
+};
+const worktimeline = [
+  {
+    jobtitle: "Rakuten Inc.",
+    where: "Tokyo",
+    date: "2024 - till",
+  },
+  {
+    jobtitle: "Deloitte",
+    where: "India",
+    date: "12/2019 - 02/2024",
+  },
+  {
+    jobtitle: "United Health Group",
+    where: "India",
+    date: "09/2019 - 12/2019",
+  },
+  {
+    jobtitle: "Seanergy Digital",
+    where: "India",
+    date: "06/2018 - 09/2019",
+  },
+  {
+    jobtitle: "Brandpier",
+    where: "India",
+    date: "09/2016 - 05/2018",
+  },
+  {
+    jobtitle: "SureSoft Inc.",
+    where: "India",
+    date: "06/2014 - 09/2016",
+  },
+];
+
+const skills = [
+  {
+    name: "C#, Java, JavaScript, TypeScript",
+    value: 95,
+  },
+  {
+    name: "React.js, Angular.js, jQuery, Three.js",
+    value: 90,
+  },
+  {
+    name: "ASP.NET, MVC, Next.js, Node.js, Express.js, Spring Boot",
+    value: 90,
+  },
+  {
+    name: "Query Languages",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "ai25jay@gmail.com",
+  YOUR_FONE: "+81 7083801***",
+  description:
+    "I'm always excited to connect with fellow professionals, potential collaborators, and anyone passionate about technology and innovation. Whether you have a project in mind, need technical advice, or just want to network, feel free to reach out. Let's discuss how we can work together to achieve remarkable results. You can contact me via email. Looking forward to hearing from you!",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/PraJay25",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/jay-prakash-37068624/",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
